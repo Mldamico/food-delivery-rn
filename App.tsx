06e7b2ce4f1eee9e76f723a55a6b75a88d0e245d
mldamico <matias.lioneldamico@gmail.com>
2023-06-29 19:40:12 -0300

    Login Screen basic layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/chef.jpg")} style={styles.background} />
+      <View style={styles.formContainer}></View>
     </View>
   );
 }
@@ -13,8 +13,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  background: {
+    width: "100%",
+    height: "100%",
+    opacity: 0.7,
+    bottom: "30%",
+  },
+  formContainer: {
+    backgroundColor: "#fff",
+    width: "100%",
+    height: "40%",
+    position: "absolute",
+    bottom: 0,
+    borderTopStartRadius: 40,
+    borderTopEndRadius: 40,
   },
 });
